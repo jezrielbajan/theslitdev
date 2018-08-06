123bb8f16958f72ad909ea5be18cafbc22461b6a
jezrielbajan <jezrielbajan@gmail.com>
2018-08-06 15:16:22 +0800

    Updates

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/JEZRIEL/Desktop/activ/theslitdev/src/layouts/index.js"
-  import data from "/Users/JEZRIEL/Desktop/activ/theslitdev/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
